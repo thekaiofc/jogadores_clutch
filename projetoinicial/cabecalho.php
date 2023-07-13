c28e2c6ccd7dbfc3f6dce3a48c15a3360ddcb928
thekaiofc <kaio.hen13@gmail.com>
2023-07-13 19:21:28 -0300

    adicionando menu

---
+++ projetoinicial/cabecalho.php
@@ -0,0 +1,9 @@
+<html>
+
+<head>
+    <title>JOGADORES CLUTCH</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <header>
+        <h1>JOGADORES CLUTCH</h1>
+    </header>
+</head>
---
+++ projetoinicial/rodape.php
@@ -0,0 +1,6 @@
+</section>
+<br>
+<footer> &copy JOGADORES CLUTCH - 2023 </footer>
+</body>
+
+</html>
