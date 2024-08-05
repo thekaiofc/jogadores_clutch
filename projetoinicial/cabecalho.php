7292fc65f7ad1580457cdcf1b886b85941a18e0b
thekaiofc <kaio.hen13@gmail.com>
2024-08-04 21:59:00 -0300

    ajustes

--- projetoinicial/cabecalho.php
+++ projetoinicial/cabecalho.php
@@ -3,6 +3,7 @@
 <head>
     <title>JOGADORES CLUTCH</title>
     <link rel="stylesheet" type="text/css" href="styles.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <header>
         <h1>JOGADORES CLUTCH</h1>
     </header>
